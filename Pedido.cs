8c0b1225419f82a28663d3970558e565d66c854d
ieelly <raniiellyfernandes@gmail.com>
2026-06-22 20:50:22 -0300

    Adicionar arquivos de projeto.

---
+++ Pedido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeteriaAromaGrao
+{
+    public class Pedido
+    {
+        public double CalcularTotal(double valor, int quantidade)
+        {
+            return valor * quantidade;
+        }
+
+        public double AplicarDesconto(double total)
+        {
+            if (total >= 100)
+                return total * 0.9;
+
+            return total;
+        }
+
+        public string StatusPedido(double total)
+        {
+            if (total < 20)
+                return "Pequeno";
+
+            if (total < 100)
+                return "Médio";
+
+            return "Grande";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CafeteriaAromaGrao;
+
+Pedido pedido = new Pedido();
+
+double total = pedido.CalcularTotal(10, 5);
+
+Console.WriteLine($"Total: R${total}");
+
+double totalComDesconto = pedido.AplicarDesconto(total);
+
+Console.WriteLine($"Total com desconto: R${totalComDesconto}");
+
+Console.WriteLine($"Status: {pedido.StatusPedido(totalComDesconto)}");
